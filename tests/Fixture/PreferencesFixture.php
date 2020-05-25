639904bc014a55a2b95322ad1c490c9e780e7198
dreamingmind <ddrake@dreamingmind.com>
2020-05-25 11:00:47 -0700

    rebake the fixture

--- tests/Fixture/PreferencesFixture.php
+++ tests/Fixture/PreferencesFixture.php
@@ -43,10 +43,12 @@ class PreferencesFixture extends TestFixture
 //                'id' => 1,
 //                'prefs' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
 //                'user_id' => 1,
-//                'created' => '2020-05-25 05:53:09',
-//                'modified' => '2020-05-25 05:53:09',
+//                'created' => '2020-05-25 17:28:37',
+//                'modified' => '2020-05-25 17:28:37',
 //            ],
 //        ];
 //        parent::init();
     }
+
+
 }
